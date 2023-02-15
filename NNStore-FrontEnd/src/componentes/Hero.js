8760382dd356b49a813b340c1de3483a2d9abd96
Nicolle <nacosta.h2001@gmail.com>
2023-02-14 21:29:59 -0500

    added hero

---
+++ NNStore-FrontEnd/src/componentes/Hero.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+
+export default function Hero(){
+    return(
+        <div className="hero">
+            <h2>¡30 días de entrega gratis!</h2>
+            <p>Para tus primeros pedidos en Restaurantes.</p>
+        </div>
+    );
+}
