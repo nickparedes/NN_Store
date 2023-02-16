dfcf6acef0bfa1ee311f67bf29afd6719cab677b
nickparedes <nick.paredes@unmsm.edu.pe>
2023-02-16 17:15:27 -0500

    add change

--- NNStore-FrontEnd/src/pages/Home.js
+++ NNStore-FrontEnd/src/pages/Home.js
@@ -1,10 +1,9 @@
 import React from "react"
-import Hero from "../componentes/Hero";
+
 
 export default function Home(){
     return(
-        <div className="home">
-            <Hero/>
+        <div>
             <h2>Soy la Home</h2>
         </div>
     );
