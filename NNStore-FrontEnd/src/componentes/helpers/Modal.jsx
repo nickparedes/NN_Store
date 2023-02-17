9fd438d90f0c068e87c4c5a04303190e75b3435b
nickparedes <nick.paredes@unmsm.edu.pe>
2023-02-17 03:52:19 -0500

    implements logins and register

---
+++ NNStore-FrontEnd/src/componentes/helpers/Modal.jsx
@@ -0,0 +1,60 @@
+import React, { Component } from 'react'
+import Portal from "./Portal"
+
+const styles = {
+    wrapper :{
+        position: "absolute",
+        top:0,
+        left: 0,
+        width: "100%",
+        height: "100%",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center"
+    },
+
+    window : {
+        position: "relative",
+        borderRadius: 5,
+        zIndex: 10,
+        minWidth: 320,
+    },
+    closeBtn:{
+        position: "absolute",
+        top: 0,
+        right: 0,
+    },
+    background:{
+        position: "absolute",
+        width: "100%",
+        height:"100%",
+
+        background: "#000",
+        opacity: 0.4,
+    }
+
+}
+
+
+export default class modal extends Component {
+  render() {
+    
+    const { children, toggle, active} = this.props;
+    
+    return (
+      <Portal>
+        {active &&(
+            <div style={styles.wrapper}>
+                <div style={styles.window}>
+                    <button style={styles.closeBtn} onClick={toggle}>x</button>
+                    <div>{children}</div>
+                </div>
+                <div onClick={toggle} style = {styles.background}> </div>
+            </div>
+        )}
+
+
+      </Portal>
+    )
+  }
+}
